24439f1ee255066c3220dd8a173f3399af012fef
Prokop Simek <prokop@dxheroes.io>
2024-06-18 18:14:37 +0200

    chore: lint and export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         domains: ['repository-images.githubusercontent.com', "opengraph.githubassets.com"],
     },
